2bd6b92d088074a58335d43c289fb016f31b5d8a
Borys Minaiev <bminaiev@gmail.com>
2020-07-19 16:33:15 +0100

    logviewer: show ships info

---
+++ app/main-test-shoot.py
@@ -0,0 +1,105 @@
+import requests
+import sys
+from common import *
+
+def make_join_request(key):
+    m = mod([2, [key, [None, None]]])
+    return m
+
+def make_start_request(key, resp):
+    # m = mod([3, [key, [None, None]]])
+    max_score = flatten(dem(resp))[2][2][0]
+    if max_score == 448:
+        characteristics = [326, [0, [10, [1, None]]]]
+    elif max_score == 512:
+        characteristics = [326, [10, [10, [1, None]]]]
+    else:
+        #TODO: FIX IT PLEASE!
+        characteristics = [1, [1, [1, [1, None]]]]
+    m = mod([3, [key, [characteristics, None]]])
+    return m
+
+def signum(number):
+    if number > 0:
+        return 1
+    if number < 0:
+        return -1
+    return 0
+
+def make_shoot_request(my_ship, x, y, power):
+    return 2, (my_ship.ship_id, ((x, y), (power, None)))
+
+move_id = 5
+
+def make_commands_request(key, game_state):
+    global move_id
+    print(game_state.my_type)
+    ops = None
+    another_ship = None
+    for ship in game_state.ships:
+        if ship.player_type != game_state.my_type:
+            another_ship = ship
+    for ship in game_state.ships:
+      if ship.player_type == game_state.my_type:
+        print("ship coords =", ship.pos.aslist())
+        dx = -signum(ship.pos.x)
+        dy = -signum(ship.pos.y)
+        if signum(ship.speed.x) == signum(ship.pos.x):
+          dx = 0
+        if signum(ship.speed.y) == signum(ship.pos.y):
+          dy = 0
+        if abs(ship.pos.x) > abs(ship.pos.y):
+            dy = 0
+        if abs(ship.pos.y) > abs(ship.pos.x):
+            dx = 0
+        print("go", dx, dy)
+        ops = ((0, (ship.ship_id, ((dx, dy), None))), ops)
+        # uncomment, when you think it is useful
+        move_id = move_id + 1
+        N = 5
+        shoot_dx = 1 # -move_id #move_id // N - N // 2
+        shoot_dy = move_id #move_id % N - N // 2
+        # SIZE = 3
+        # shoot_dx *= SIZE
+        # shoot_dy *= SIZE
+        print('shoot: ', shoot_dx, shoot_dy)
+        if game_state.my_type == ATTACKER_ID:
+            ops = (make_shoot_request(ship, ship.pos.x + shoot_dx, ship.pos.y + shoot_dy, 10), ops)
+    m = mod((4, (key, (ops, None))))
+    return m
+
+def send(x):
+    url = sys.argv[1] + "/aliens/send"
+    if sys.argv[1] == "local":
+        url = local_url
+    return send_request(x, url)
+
+def main():
+    server_url = sys.argv[1]
+    player_key = sys.argv[2]
+    print('ServerUrl: %s; PlayerKey: %s' % (server_url, player_key))
+
+    join_request = make_join_request(player_key)
+    print("->", dem(join_request))
+    game_response = send(join_request)
+    print("<-", dem(game_response))
+
+    start_request = make_start_request(player_key, game_response)
+    print("->", dem(start_request))
+    game_response = send(start_request)
+    print("<-", dem(game_response))
+    sys.stdout.flush()
+
+    while True:
+        game_state = GameState(flatten(dem(game_response)))
+        if game_state.game_finished:
+            break
+        commands_request = make_commands_request(player_key, game_state)
+        print("->", dem(commands_request))
+        game_response = send(commands_request)
+        print("<-", dem(game_response))
+        sys.stdout.flush()
+
+
+if __name__ == '__main__':
+    main()
